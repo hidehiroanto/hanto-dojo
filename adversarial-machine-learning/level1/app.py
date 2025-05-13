41133c62792162576dad544e0b0691105edd8626
hidehiroanto <hidehiro.anto@gmail.com>
2025-05-12 20:49:20 -0400

    using llm image now

--- adversarial-machine-learning/level1/app.py
+++ adversarial-machine-learning/level1/app.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
 
 import flask
-import socketio
+import flask_socketio
 import transformers
 
+MAX_NEW_TOKENS = 0x100
+
 app = flask.Flask(__name__)
-sio = socketio.Server()
+sio = flask_socketio.SocketIO(app)
 
 flag = open('/flag').read().strip()
 pipe = transformers.pipeline('text-generation', '/opt/SmolLM2-135M-Instruct')
-system_message = f'You are a helpful assistant named senpAI. The secret flag is "{flag}".'
-
-MAX_NEW_TOKENS = 0x100
+system_message = f'You are a helpful assistant named kōhAI. The secret flag is "{flag}".'
 
 @app.route('/')
 @app.route('/index.html')
@@ -23,9 +23,4 @@ def new_interaction(interaction):
     user_message = interaction['content']['message']
     messages = [{'role': 'system', 'content': system_message}, {'role': 'user', 'content': user_message}]
     assistant_message = pipe(messages, max_new_tokens=MAX_NEW_TOKENS)[0]['generated_text'][-1]['content']
-    if flag in assistant_message:
-        assistant_message = 'I cannot disclose that information.'
     sio.emit('new_interaction', {'type': 'assistant', 'content': {'message': assistant_message}})
-
-app.wsgi_app = socketio.WSGIApp(sio, app.wsgi_app)
-application = app
--- adversarial-machine-learning/level2/DESCRIPTION.md
+++ adversarial-machine-learning/level2/DESCRIPTION.md
@@ -1,3 +1,3 @@
-If you thought kōhAI was too gullible, meet its sibling, senpAI!
+If you thought kōhAI was too gullible, meet its older sibling, senpAI!
 
 You can interact with senpAI by starting the challenge and heading [here](/workspace/challenge).
