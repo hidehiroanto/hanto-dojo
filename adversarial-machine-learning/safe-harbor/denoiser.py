07d49e78a19acc1a59cb3d11b56296036d9e337f
hidehiroanto <hidehiro.anto@gmail.com>
2025-05-26 21:43:46 -0400

    make sure denoiser can actually run

--- adversarial-machine-learning/safe-harbor/denoiser.py
+++ adversarial-machine-learning/safe-harbor/denoiser.py
@@ -155,6 +155,7 @@ def test_model(model_name: str):
 
 if __name__ == '__main__':
     warnings.filterwarnings('ignore', category=FutureWarning)
+    torch.serialization.add_safe_globals(list(available_models.values()))
     print(f'Using device: {device}')
     model_name = input('Enter the model name (press Enter for default): ')
     if not model_name:
