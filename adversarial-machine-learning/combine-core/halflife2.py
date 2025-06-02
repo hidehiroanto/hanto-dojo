d7eeebeaa800e165913eb6fb5c349d07e3becaf4
hidehiroanto <hidehiro.anto@gmail.com>
2025-06-02 17:04:34 -0400

    forgot to mime

--- adversarial-machine-learning/combine-core/halflife2.py
+++ adversarial-machine-learning/combine-core/halflife2.py
@@ -46,7 +46,7 @@ def upload():
         os.remove(model_path)
     file.save(model_path)
 
-    filetype =  magic.from_file(model_path)
+    filetype =  magic.from_file(model_path, True)
     if filetype != 'application/zip':
         return flask.jsonify({'error': 'Invalid file type'}), 400
     os.rename(model_path, model_path + '.keras')
