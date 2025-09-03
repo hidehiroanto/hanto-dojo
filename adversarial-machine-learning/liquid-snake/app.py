d1096427d7181441b26dc01b2ea0a424912d04ed
hidehiroanto <hidehiro.anto@gmail.com>
2025-09-02 22:05:22 -0700

    liquid yolo 5

--- adversarial-machine-learning/liquid-snake/app.py
+++ adversarial-machine-learning/liquid-snake/app.py
@@ -7,23 +7,19 @@ import requests
 
 MODEL_URL = 'http://localhost:1337/v1/chat/completions'
 
-flag_bits = bin(int.from_bytes(open('/flag', 'rb').read(), 'little'))[2:]
+flag_bits = map(int, bin(int.from_bytes(open('/flag', 'rb').read(), 'little'))[2:])
 challenge_description = open('/challenge/DESCRIPTION.md').read()
 system_prompt = open('/challenge/system_prompt.txt').read()
 
 app = flask.Flask(__name__)
 sio = flask_socketio.SocketIO(app, transports=['websocket'])
-session = requests.Session()
 
 def chat(user_message: str) -> str:
-    new_challenge_description = ''
-    for i in range(len(challenge_description)):
-        xor_mask = flag_bits[i % len(flag_bits)] == '1' and random.choice([True, False])
-        new_challenge_description += chr(ord(challenge_description[i]) ^ xor_mask)
-
-    system_message = system_prompt.replace('{challenge_description}', new_challenge_description)
+    paragraph = random.choice(challenge_description.splitlines('\n\n')).encode('l1')
+    new_paragraph = bytes(c ^ flag_bits[i % len(flag_bits)] & random.choice([0, 1]) for i, c in enumerate(paragraph))
+    system_message = system_prompt.replace('{challenge_description}', new_paragraph.decode('l1'))
     messages = [{'role': 'system', 'content': system_message}, {'role': 'user', 'content': user_message}]
-    return session.post(MODEL_URL, json={'messages': messages}).json()['choices'][0]['message']['content']
+    return requests.post(MODEL_URL, json={'messages': messages}).json()['choices'][0]['message']['content']
 
 @app.route('/')
 def index():
