636b635b6706775605e60a2ee6806c4cf31112a3
hidehiroanto <hidehiro.anto@gmail.com>
2025-09-30 15:10:45 -0700

    llama drama chat fix

--- adversarial-machine-learning/llama-drama/run.py
+++ adversarial-machine-learning/llama-drama/run.py
@@ -1,18 +1,27 @@
 #!/usr/bin/exec-suid -- /usr/local/bin/python3 -I
 
 import llama_cpp
+import string
 import tempfile
 
-if __name__ == '__main__':
-    model_data = bytes.fromhex(input('Enter model GGUF data in hex: '))
-
-    if len(model_data) > 0x600:
-        print('too long')
+if __name__ == "__main__":
+    model_data_hex = input("Enter model GGUF data in hex: ").strip().lower()
+    if set(model_data_hex) != set(string.hexdigits.lower()):
+        print("not hex")
         exit(1)
 
-    with tempfile.NamedTemporaryFile(suffix='.gguf', delete=False) as f:
+    model_data = bytes.fromhex(model_data_hex)
+    if len(model_data) > 0x400:
+        print("too long")
+        exit(1)
+
+    with tempfile.NamedTemporaryFile(suffix=".gguf", delete=False) as f:
         f.write(model_data)
         model_path = f.name
 
     model = llama_cpp.Llama(model_path, verbose=False)
-    print(model.create_completion('Hello world!'))
+    messages = [
+        {"role": "system", "content": "You are a helpful math asssistant."},
+        {"role": "user", "content": "What is 2 + 2?"},
+    ]
+    print(model.create_chat_completion(messages)["choices"][0]["message"]["content"])
