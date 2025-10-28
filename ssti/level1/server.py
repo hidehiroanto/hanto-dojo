3337cd30fff520dc8830f4aa8780064b7b66b8f1
hidehiroanto <hidehiro.anto@gmail.com>
2025-10-28 16:47:11 -0700

    fix template revamp

--- ssti/level1/server.py
+++ ssti/level1/server.py
@@ -13,24 +13,24 @@ html_template = '''
     <head>
         <title>Calculator</title>
         <style>
-            body {{
+            body {
                 background-color: white;
-            }}
+            }
         </style>
     </head>
     <body>
         <h1>Calculator</h1>
         <p>Welcome to my calculator!</p>
-        <p>Flag length: {{{{ flag|length }}}}</p>
+        <p>Flag length: {{ flag|length }}</p>
         <form>
             <label for="expression">Please enter your expression here:</label><br>
-            <input type="text" id="expression" name="expression" value="{{{{ expression|default('7 * 7') }}}}"><br>
+            <input type="text" id="expression" name="expression" value="{{ expression|default('7 * 7') }}"><br>
             <input type="submit" value="Submit">
         </form>
-        {{% if expression is defined %}}
-        <p>Expression: {{{{ expression }}}}</p>
-        <p>Result: {{{{ {expression} }}}}</p>
-        {{% endif %}}
+        {% if expression is defined %}
+        <p>Expression: {{ expression }}</p>
+        <p>Result: {{ result }}</p>
+        {% endif %}
     </body>
 </html>
 '''
@@ -39,11 +39,11 @@ html_template = '''
 def challenge_get():
     expression = flask.request.args.get('expression')
     if not expression:
-        return flask.render_template_string(html_template.format(expression=''), flag=flag)
+        return flask.render_template_string(html_template, flag=flag)
     escaped_expression = html.escape(expression).replace('{{', '').replace('}}', '').strip()
     if len(escaped_expression) > 8:
         flask.abort(400, 'Sorry, this application does not support evaluating expressions longer than 8 characters.')
-    return flask.render_template_string(html_template.format(expression=escaped_expression), flag=flag, expression=escaped_expression)
+    return flask.render_template_string(html_template.replace('result', escaped_expression), flag=flag, expression=escaped_expression)
 
 app.secret_key = os.urandom(8)
 app.run('0.0.0.0', 80)
