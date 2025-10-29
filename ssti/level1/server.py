e75ab9804a2b5a4630023f23c5955e843703feb4
hidehiroanto <hidehiro.anto@gmail.com>
2025-10-28 17:06:27 -0700

    Add error template

--- ssti/level1/server.py
+++ ssti/level1/server.py
@@ -7,31 +7,39 @@ import os
 app = flask.Flask(__name__)
 flag = open('/flag').read().strip()
 
-html_template = '''
+html_200 = '''
 <!doctype html>
-<html lang=en>
-    <head>
-        <title>Calculator</title>
-        <style>
-            body {
-                background-color: white;
-            }
-        </style>
-    </head>
-    <body>
-        <h1>Calculator</h1>
-        <p>Welcome to my calculator!</p>
-        <p>Flag length: {{ flag|length }}</p>
-        <form>
-            <label for="expression">Please enter your expression here:</label><br>
-            <input type="text" id="expression" name="expression" value="{{ expression|default('7 * 7') }}"><br>
-            <input type="submit" value="Submit">
-        </form>
-        {% if expression is defined %}
-        <p>Expression: {{ expression }}</p>
-        <p>Result: {{ result }}</p>
-        {% endif %}
-    </body>
+<html>
+<head>
+    <title>Calculator</title>
+</head>
+<body style="background-color: white">
+    <h1>Calculator</h1>
+    <p>Welcome to my calculator!</p>
+    <p>Flag length: {{ flag|length }}</p>
+    <form>
+        <label for="expression">Please enter your expression here:</label><br>
+        <input type="text" id="expression" name="expression" value="{{ expression|default('7 * 7') }}"><br>
+        <input type="submit" value="Submit">
+    </form>
+    {% if expression is defined %}
+    <p>Expression: {{ expression }}</p>
+    <p>Result: {{ result }}</p>
+    {% endif %}
+</body>
+</html>
+'''
+
+html_400 = '''
+<!doctype html>
+<html>
+<head>
+    <title>400 Bad Request</title>
+</head>
+<body style="background-color: white">
+    <h1>Bad Request</h1>
+    <p>{{ error_message }}</p>
+</body>
 </html>
 '''
 
@@ -39,11 +47,11 @@ html_template = '''
 def challenge_get():
     expression = flask.request.args.get('expression')
     if not expression:
-        return flask.render_template_string(html_template, flag=flag)
+        return flask.render_template_string(html_200, flag=flag)
     escaped_expression = html.escape(expression).replace('{{', '').replace('}}', '').strip()
     if len(escaped_expression) > 8:
-        flask.abort(400, 'Sorry, this application does not support evaluating expressions longer than 8 characters.')
-    return flask.render_template_string(html_template.replace('result', escaped_expression), flag=flag, expression=escaped_expression)
+        return flask.render_template_string(html_400, error_message='Sorry, this application does not support evaluating expressions longer than 8 characters.'), 400
+    return flask.render_template_string(html_200.replace('result', escaped_expression), flag=flag, expression=escaped_expression)
 
 app.secret_key = os.urandom(8)
 app.run('0.0.0.0', 80)
